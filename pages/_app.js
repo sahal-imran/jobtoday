8cdb241f41befe324e783c7605f912638814d562
Sahal Imran <sahalimran786@gmail.com>
2022-07-14 14:02:24 +0500

    error set attempt 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import "../styles/globals.css";
-import "../pages/i18nextConf";
 import React, { Suspense } from "react";
 import Layout from "../pages/layout";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "ar", "fr"],
+    fallbackLng: "en",
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ["path", "cookie", "htmlTag"],
+      caches: ["cookie"],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
 
 function MyApp({ Component, pageProps }) {
   return (
